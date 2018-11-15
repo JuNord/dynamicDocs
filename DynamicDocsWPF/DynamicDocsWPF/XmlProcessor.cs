cfa324b222a6dc058828f7a758392576d0a51e4c
JuNord <45033862+junord@users.noreply.github.com>
2018-11-15 09:37:24 +0100

    Anlegen eines XML Reader Projektes

---
+++ DynamicDocsWPF/DynamicDocsWPF/XmlProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an XML reader for this file.
+            using (XmlReader reader = XmlReader.Create(@"C:\Users\Julius.Nordhues\source\repos\dynamicDocs\XML_Test\XMLFile1.xml"))
+            {
+                while (reader.Read())
+                {
+                    // Only detect start elements.
+                    if (reader.IsStartElement())
+                    {
+                        // Get element name and switch on it.
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                Console.WriteLine("Start Element {0}", reader.Name);
+                                break;
+                            
+                            case XmlNodeType.EndElement:
+                                Console.WriteLine("End Element {0}", reader.Name);
+                                break;
+                            default:
+                                Console.WriteLine("Other node {0} with value {1}",
+                                                reader.NodeType, reader.Value);
+                                break;
+                        }
+
+                    }
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
