c39cbd0b9686c16e10f9394a458d87096062421e
JuNord <45033862+junord@users.noreply.github.com>
2018-11-19 10:43:49 +0100

    Weiteres bearbeiten der Tagauslese

--- DynamicDocsWPF/XmlProcessor/Program.cs
+++ DynamicDocsWPF/XmlProcessor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Xml;
 
 namespace XmlProcessor
@@ -22,13 +23,39 @@ namespace XmlProcessor
                                 if (reader.HasAttributes)
                                 {
                                     Console.WriteLine("Attributes of <" + reader.Name + ">");
-                                    string tmp = reader.GetAttribute("name");
-                                    Console.WriteLine("\tname: " + tmp );
+                                    string name = reader.GetAttribute("name");
+                                    if(name!=null)
+                                        Console.WriteLine("\tname: " + name );
                                    
-                                    string tmp1 = reader.GetAttribute("description");
-                                    Console.WriteLine("\tdescription: " + tmp1 );
-     
+                                    string description = reader.GetAttribute("description");
+                                    if(description!=null)
+                                        Console.WriteLine("\tdescription: " + description );
+
                                     
+                                    string target = reader.GetAttribute("target");
+                                    if(target!=null)
+                                        Console.WriteLine("\ttarget: "+target);
+                                         
+                                    string locks = reader.GetAttribute("locks");
+                                    if(locks!=null)
+                                        Console.WriteLine("\tlocks: "+locks);
+                                    
+                                    string vText = reader.GetAttribute("text");
+                                    if(vText!=null)
+                                        Console.WriteLine("\ttext: "+vText);
+                                    
+                                    string draftname = reader.GetAttribute("draftname");
+                                    if(draftname!=null)
+                                        Console.WriteLine("\tdraftname: "+draftname);
+                                    
+                                    string filepath = reader.GetAttribute("filepath");
+                                    if(filepath!=null)
+                                        Console.WriteLine("\tfilepath: "+filepath);
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Attributes of <" + reader.Name + ">");
                                 }
                                 // Move the reader back to the element node.
                                     //reader.MoveToElement(); 
